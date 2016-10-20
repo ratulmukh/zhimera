c4de8894ecc98f50a1ea354121f25911d61d688e
Ratul Mukhopadhyay <ratul.mukh@gmail.com>
2016-10-20 05:09:25 +0000

    some basic files

---
+++ src/main/java/Comment.java
@@ -0,0 +1,27 @@
+package ratul.myexperiments;
+
+class Comment {
+    private final String commentContent;
+    private final User userWhoCommented;
+    private final Video commentedOnVideo;
+    private final Comment parentComment;
+    private int upVoteCount = 0;
+    private int downVoteCount = 0;
+    
+    public Comment(String commentContent, User userWhoCommented, Video commentedOnVideo, Comment parentComment) {
+        this.commentContent = commentContent;
+        this.userWhoCommented = userWhoCommented;
+        this.commentedOnVideo = commentedOnVideo;
+        this.parentComment = parentComment;
+    }
+    
+    public void incrementUpVoteCount() {
+        upVoteCount++;
+    }
+    
+    public void decrementUpVoteCount() {
+        upVoteCount--;
+    }
+    
+    public
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,5 @@
+package ratul.myexperiments;
+
+class User {
+    
+}

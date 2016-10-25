6c6a053d44acefa55091148c2aa0d0a3a5429fa7
Ratul Mukhopadhyay <ratul.mukh@gmail.com>
2016-10-25 06:07:09 +0000

    attempt at adding scheduler to client

--- src/main/java/ratul/experiments/cluster/VideoStorageClient.java
+++ src/main/java/ratul/experiments/cluster/VideoStorageClient.java
@@ -2,8 +2,17 @@ package ratul.myexperiments.cluster;
 
 import ratul.myexperiments.cluster.ClusterMsgs.*;
 import akka.actor.*;
+import scala.concurrent.duration.Duration;
+import java.util.concurrent.TimeUnit;
+
 
 public class VideoStorageClient extends UntypedActor {
+
+  ActorRef p = getContext().actorSelection("/user/video-storage-coordinator-proxy").resolveOne();
+  Cancellable cancellable = getContext().system().scheduler().schedule(Duration.Zero(),
+    Duration.create(1000, TimeUnit.MILLISECONDS), p, 
+    new Hello(), getContext().system().dispatcher(), null);
+
 
 
   @Override
@@ -11,6 +20,7 @@ public class VideoStorageClient extends UntypedActor {
     if (message instanceof Hello) {
       Hello job = (Hello) message;
       getSender().tell(new HelloResult(), getSelf());
+      //getContext().actorSelection("/user/video-storage-coordinator-proxy").tell(new HelloResult(), getSelf());
     }
     else {
       unhandled(message);
